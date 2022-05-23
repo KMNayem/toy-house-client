4e2d654a85b0e7e80e81748128871eb394dae2de
KMNayem <imna41695@gmail.com>
2022-05-23 21:22:45 +0600

    error

--- src/Pages/Auth/Register/Register.js
+++ src/Pages/Auth/Register/Register.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+
+
+  // const navigate = useNavigate();
+
+
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const confirmPasswordRef = useRef('');
+
+  const handleFromInput = event =>{
+    event.preventDefault();
+    // console.log(event.target)
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+    console.log(email, password, confirmPassword);
+  
+    createUserWithEmailAndPassword(email, password, confirmPassword);
+  }
+
+
     return (
         <div className='container w-50 mx auto'>
                 <h1 className='text-center'>Please Register</h1>
-                <Form>
+                <Form onSubmit={handleFromInput}>
                      <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                      </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control ref={confirmPasswordRef} type="password" placeholder="Password" />
                      </Form.Group>
 
                      <p className="my-3">
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,3 +1,32 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/auth';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // const firebaseConfig = {
+
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyDOu3TZJSzKFg_h5jt7JisGwPANxsJ-wkY",
+//     authDomain: "toy-house-b1a2d.firebaseapp.com",
+//     projectId: "toy-house-b1a2d",
+//     storageBucket: "toy-house-b1a2d.appspot.com",
+//     messagingSenderId: "168035248177",
+//     appId: "1:168035248177:web:9db2001df3e7b2b31c4d10"
+//   };
+
+//   // apiKey: process.env.REACT_APP_apiKey,
+//   // authDomain: process.env.REACT_APP_authDomain,
+//   // projectId: process.env.REACT_APP_projectId,
+//   // storageBucket:process.env.REACT_APP_storageBucket,
+//   // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//   // appId: process.env.REACT_APP_appId
+// // };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
@@ -6,12 +35,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  apiKey: "AIzaSyDOu3TZJSzKFg_h5jt7JisGwPANxsJ-wkY",
+  authDomain: "toy-house-b1a2d.firebaseapp.com",
+  projectId: "toy-house-b1a2d",
+  storageBucket: "toy-house-b1a2d.appspot.com",
+  messagingSenderId: "168035248177",
+  appId: "1:168035248177:web:9db2001df3e7b2b31c4d10"
 };
 
 // Initialize Firebase
