1f31177bf6b4cface0e14824fa6d799a57a4cdf4
KMNayem <imna41695@gmail.com>
2022-05-24 20:17:36 +0600

    auth

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Blogs from './Pages/Blogs/Blogs';
 import Login from './Pages/Auth/Login/Login';
 import Footer from './Pages/Shared/Footer/Footer';
 import Register from './Pages/Auth/Register/Register';
+import RequireAuth from './Pages/Auth/RequireAuth/RequireAuth';
+import ProductDetails from './Pages/ProductDetails/ProductDetails';
 
 function App() {
   return (
@@ -18,6 +20,14 @@ function App() {
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route
+          path="/product/:productId"
+          element={
+            <RequireAuth>
+              <ProductDetails></ProductDetails>
+            </RequireAuth>
+          }
+        ></Route>
       </Routes>
       <Footer/>
     </div>
--- src/Pages/Auth/Login/Login.js
+++ src/Pages/Auth/Login/Login.js
@@ -1,19 +1,43 @@
 import React, { useRef } from 'react'
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 function Login() {
 
         const emailRef = useRef("");
         const passwordRef = useRef("");
+        const navigate = useNavigate();
+        const location = useLocation();
 
+        let from = location.state?.from?.pathname || "/";
+
+        let errorElement;
+    const [
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+     if (user) {
+      navigate(from, { replace: true });
+    }
+
+     if (error) {
+      errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
         const handleSubmit = event => {
             event.preventDefault();
             const email = emailRef.current.value;
             const password = passwordRef.current.value;
             console.log(email, password );
         }
+
+        const navigateRegister = event => {
+          navigate('/register');
+      }
 
     return (
 
@@ -50,11 +74,12 @@ function Login() {
               <h6 className="my-3">
                 Don't have a account?
                 <span>
-                  <Link to="/register" className="text-color">
+                  <Link to="/register" onClick={navigateRegister} className="text-color">
                     Register
                   </Link>
                 </span>
               </h6>
+              {errorElement}
                  <Button className='btn card-button rounded-pill' type="submit">
                  Login
                      </Button>
